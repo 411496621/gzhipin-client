3c9e1d538e0923f9a48d02a4803b7c69a38d28bd
haoyun <411496621@qq.com>
2018-12-03 20:09:20 +0800

    完成注册和登录组件

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,13 +1,30 @@
 import React,{Component} from "react"
-
+import {NavBar,List,InputItem,WhiteSpace,WingBlank,Button} from "antd-mobile"
+import Logo from "../logo"
 class Login extends Component{
 
+  goRegister = ()=>{
+     this.props.history.replace("/register")
+  }
 
   render(){
 
     return(
       <div>
-        Login
+        <NavBar>硅谷直聘</NavBar>
+        <Logo/>
+        <WingBlank>
+          <List>
+            <InputItem>用户名:</InputItem>
+            <WhiteSpace />
+            <InputItem type="password">密&nbsp;&nbsp;&nbsp;码:</InputItem>
+            <WhiteSpace />
+            <Button type="primary">登录</Button>
+            <WhiteSpace />
+            <Button onClick={this.goRegister}>还没有账户</Button>
+          </List>
+        </WingBlank>
+
       </div>
     )
   }
--- src/components/register/index.jsx
+++ src/components/register/index.jsx
@@ -1,13 +1,53 @@
 import React,{Component} from "react"
+import {NavBar,WingBlank,List,InputItem,WhiteSpace,Radio,Button} from "antd-mobile"
+import  Logo from "../logo"
 
+const {Item} = List
 class Register extends Component{
+  state ={
+    isBoss:true,
+    username:"",
+    pwd:"",
+    rePwd:""
+  }
+  hasChange =(type,value)=>{
+       this.setState({
+         [type]:value
+       })
+  }
+  messageRegister = ()=>{
+    const {isBoss,username,pwd,rePwd} =this.state
+    console.log(isBoss,username,pwd,rePwd)
+  }
+  goLogin = ()=>{
+     this.props.history.replace("/login")
+  }
 
 
   render(){
-
+    const {isBoss} = this.state
     return(
       <div>
-        Register
+        <NavBar>硅谷直聘</NavBar>
+        <Logo/>
+        <WingBlank>
+          <List>
+            <InputItem onChange={ value =>this.hasChange("username",value) }>用户名:</InputItem>
+            <WhiteSpace/>
+            <InputItem type="password" onChange={value => this.hasChange("pwd",value) }>密 &nbsp;&nbsp;码:</InputItem>
+            <WhiteSpace/>
+            <InputItem type="password" onChange={value => this.hasChange("rePwd",value)} >确认密码:</InputItem>
+            <WhiteSpace/>
+            <Item>用户类型:
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<Radio onChange={this.hasChange.bind(null,"isBoss",false) } checked={!isBoss} >大神</Radio>
+              &nbsp;&nbsp;&nbsp;<Radio onChange={this.hasChange.bind(null,"isBoss",true) } checked={isBoss}>老板</Radio>
+            </Item>
+            <WhiteSpace/>
+            <Button type="primary" onClick={this.messageRegister} >注册</Button>
+            <WhiteSpace/>
+            <Button onClick={this.goLogin}>已有账户</Button>
+          </List>
+        </WingBlank>
       </div>
     )
   }
